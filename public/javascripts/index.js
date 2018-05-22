fa116bdf3cf2ff0e93b7080a6ca565533e91e033
AndsLog <leanjn0922@gmail.com>
2018-05-22 22:37:45 +0800

    test v2

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,12 @@
-return function () {
-    $(document).ready(function() {
-        $(documnet).on(click, '#submit', getValue);
-    });
+// return function () {
+// }
 
-    function getValue(){
-        let eMail = $('#e-mail').val();
-        let password = $('#password').val();
-        console.log("e-mail:" + eMail + " password:" + password);
-    }
+$(document).ready(function() {
+    $(documnet).on(click, '#submit', getValue);
+});
+
+function getValue(){
+    let eMail = $('#e-mail').val();
+    let password = $('#password').val();
+    console.log("e-mail:" + eMail + " password:" + password);
 }
