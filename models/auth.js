bbeda010a9ec345d9aab69ab32857b1a5c4dbfe8
AndsLog <leanjn0922@gmail.com>
2018-05-25 13:58:35 +0800

    recorrect ref

--- models/auth.js
+++ models/auth.js
@@ -4,7 +4,7 @@
     function AuthModel() {};
 
     AuthModel.prototype.insert = function(insertUserData){
-        return firebase.ref('users/').push().then((ref) => {
+        return firebase.database().ref('users/').push().then((ref) => {
             let userId = ref.key;
             return firebase.database().ref('users/' + userId).set(insertUserData);
         }).then(() => {
